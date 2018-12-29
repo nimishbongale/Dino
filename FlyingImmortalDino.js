8ced3abb858c791e848a3baab0cec64688dbd472
Nimish Bongale <43414361+nimishbongale@users.noreply.github.com>
2018-12-29 23:20:41 +0530

    Update FlyingImmortalDino.js

--- FlyingImmortalDino.js
+++ FlyingImmortalDino.js
@@ -1,3 +1,9 @@
+/*
+Author @nimishbongale
+Version 1.0.3
+Date 07/12/2018
+A program which makes the dino fly  
+*/
 const game=Runner.instance_;
 const tRex=game.tRex;
 
@@ -24,4 +30,4 @@ setTimeout(function() {
 }
 simulateKeyPress(32); //Autostart
 tRex.groundYPos=-1;
-alert("I'm immortal!");
+alert("I believe I can fly!");//dino gives a message 
