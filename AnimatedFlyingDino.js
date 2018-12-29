34ea56ca3d3cc7200b440dd0883d7e4878d55f49
Nimish Bongale <43414361+nimishbongale@users.noreply.github.com>
2018-12-29 23:17:02 +0530

    Update AnimatedFlyingDino.js

--- AnimatedFlyingDino.js
+++ AnimatedFlyingDino.js
@@ -1,59 +1,39 @@
+/*
+Author @nimishbongale
+Version 1.0.2
+Date 07/12/2018
+A program which makes the dino fly-jump  
+*/
 const game=Runner.instance_;
-
 const tRex=game.tRex;
-
-
-
 const dispatchKeyEvent = function(eventName, keyCode) {
 
+var e = new Event(eventName);
 
+e.keyCode = keyCode;
 
-  var e = new Event(eventName);
-
-
-
-  e.keyCode = keyCode;
-
-
-
-  document.dispatchEvent(e);
-
-
+document.dispatchEvent(e);
 
 }
-
-
-
 const simulateKeyPress = function(keyCode) {
 
-
-
-  dispatchKeyEvent('keydown', keyCode);
-
-
+dispatchKeyEvent('keydown', keyCode);
 
 setTimeout(function() {
 
+dispatchKeyEvent('keyup', keyCode);
 
-
-    dispatchKeyEvent('keyup', keyCode);
-
-
-
-  }, 300);
-
-
+}, 300);
 
 }
-
 async function dispup()
 {
-	simulateKeyPress(38);
+	simulateKeyPress(38);//press up key
 }
 
 async function disdown()
 {
-	simulateKeyPress(40);
+	simulateKeyPress(40);//press down key
 }
 
 simulateKeyPress(32); //Autostart
@@ -62,4 +42,4 @@ tRex.groundYPos=-1;
 setInterval(dispup,400);
 setInterval(disdown,800);
 
-alert("I'm immortal!");
+alert("I'm immortal!");//dino speaks in a speach bubble
