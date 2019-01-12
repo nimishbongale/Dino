82d632aa62de1b8e874d2be397d1f19aaf20c157
Nimish Bongale <43414361+nimishbongale@users.noreply.github.com>
2019-01-13 01:23:00 +0530

    Update OfflineArcadeMode.js

--- OfflineArcadeMode.js
+++ OfflineArcadeMode.js
@@ -1,3 +1,9 @@
+/*
+Author @nimishbongale
+Version 1.0.2
+Date 13/01/2019
+A program which enables you to play the dino game in arcade mode,even offline 
+*/
 const game=Runner.instance_;
 const tRex=game.trex;
 game.setArcadeMode(tRex);
