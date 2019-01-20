8300a5020c622ba0710207673ba02b5a7d49e581
Nimish Bongale <43414361+nimishbongale@users.noreply.github.com>
2019-01-21 00:06:29 +0530

    Update InvisibleDino.js

--- InvisibleDino.js
+++ InvisibleDino.js
@@ -1,3 +1,14 @@
+/*
+
+Author @nimishbongale
+
+Version 1.0.2
+
+Date 21/01/2019
+
+A program which makes the dino invisible
+
+*/
 const dispatchKeyEvent = function(eventName, keyCode) {
 
   var e = new Event(eventName);
