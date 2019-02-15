9d7de9541c735769202075255272b4d152f01257
Nimish Bongale <nimishnb98@gmail.com>
2019-02-15 21:09:14 +0530

    Update OnlyDinoPredictive.js

--- OnlyDinoPredictive.js
+++ OnlyDinoPredictive.js
@@ -1,1 +1,26 @@
+const dispatchKeyEvent = function(eventName, keyCode) {
 
+  var e = new Event(eventName);
+
+  e.keyCode = keyCode;
+
+  document.dispatchEvent(e);
+
+}
+
+const simulateKeyPress = function(keyCode) {
+
+  dispatchKeyEvent('keydown', keyCode);
+
+setTimeout(function() {
+
+    dispatchKeyEvent('keyup', keyCode);
+
+  }, 300);
+
+}
+Runner.instance_.canvas.height=140;
+Runner.instance_.canvas.width=200;
+
+simulateKeyPress(32);
+document.getElementById('main-content').click();
