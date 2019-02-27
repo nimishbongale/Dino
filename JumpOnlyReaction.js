1ea431cca1c9ded6f469f7e72670b9297470baa2
Nimish Bongale <nimishnb98@gmail.com>
2019-02-27 21:08:56 +0530

    Update JumpOnlyReaction.js

--- JumpOnlyReaction.js
+++ JumpOnlyReaction.js
@@ -1,1 +1,26 @@
+const dispatchKeyEvent = function(eventName, keyCode) {
 
+  var e = new Event(eventName);
+
+  e.keyCode = keyCode;
+
+  document.dispatchEvent(e);
+
+}
+
+const simulateKeyPress = function(keyCode) {
+
+  dispatchKeyEvent('keydown', keyCode);
+
+setTimeout(function() {
+
+    dispatchKeyEvent('keyup', keyCode);
+
+  }, 300);
+
+}
+Runner.instance_.canvas.height=70; //Modify these values to suit your needs 
+Runner.instance_.canvas.width=100;
+
+simulateKeyPress(32);
+document.getElementById('main-content').click();
